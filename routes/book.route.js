1e31448ada39b9f4b7ae2fa0040f46963c5f962b
Glitch (reliable-road-space) <none>
2020-05-11 13:02:47 +0000

    🎡🐌 Updated with Glitch

--- routes/book.route.js
+++ routes/book.route.js
@@ -1,54 +1,45 @@
 var express = require("express");
-var multer  = require('multer');
+var multer = require("multer");
 
-var upload = multer({ dest: './public/uploads/' });
+var upload = multer({ dest: "./public/uploads/" });
 
 var router = express.Router();
-var controller = require('../controllers/book.controller');
-var booksMiddleware = require('../middlewares/books.middleware');
+var controller = require("../controllers/book.controller");
+var booksMiddleware = require("../middlewares/books.middleware");
 
-router.get(
-  "/",
-  controller.index
-)
+router.get("/", controller.index);
 
-router.get(
-  "/create",
-  controller.createBook
-)
+router.get("/error", function(req, res, next) {
+  try {
+    var a;
+    a.b();
+  } catch (errors) {
+    next("this is a error :" + errors);
+  }
+});
+
+router.get("/create", controller.createBook);
 
 router.post(
   "/create",
-  upload.single('cover'),
+  upload.single("cover"),
   booksMiddleware.uploadCover,
   controller.createBookPost
-)
+);
 
-router.get(
-  "/update/:bookId", 
-  controller.updateBook
-)
+router.get("/update/:bookId", controller.updateBook);
 
-router.post(
-  "/update/:bookId",
-  controller.updateBookPost
-)
+router.post("/update/:bookId", controller.updateBookPost);
 
-router.get(
-  "/delete/:bookId",
-  controller.deleteBook
-)
+router.get("/delete/:bookId", controller.deleteBook);
 
-router.get(
-  "/update/:bookId/cover",
-  controller.updateBookCover
-)
+router.get("/update/:bookId/cover", controller.updateBookCover);
 
 router.post(
   "/update/:bookId/cover",
-  upload.single('cover'),
+  upload.single("cover"),
   booksMiddleware.uploadCover,
   controller.updateBookCoverPost
-)
+);
 
 module.exports = router;
